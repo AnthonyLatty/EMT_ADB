46c3b089653ff9f6ecc2785465e9051d52120b55
Anthony Latty <marclatty1@gmail.com>
2018-11-04 17:50:43 -0500

    WIP: adding admin specific views

--- Advanced_DB_web/Advanced_DB_web/Views/Admin/AdminDashboard.aspx
+++ Advanced_DB_web/Advanced_DB_web/Views/Admin/AdminDashboard.aspx
@@ -17,11 +17,58 @@
                 <div class="col-md-8">
                     <h1 id="banner">Welcome Admin</h1>
                     <p class="lead">Manage and review all data in system.</p>
+                    <p class="lead">
+                        <asp:Label ID="lblErrorMessage" CssClass="text-danger" Font-Bold="True" runat="server" Text=""/>
+                    </p>
                 </div>
                 <div class="col-md-4">
                     <asp:Button ID="btnLogout" runat="server" Text="Logout" class="btn btn-danger btn-lg btn-block" OnClick="btnLogout_OnClick" />
                 </div>
             </div>
+        </div>
+        <h2 style="text-align: center">Transactions</h2>
+        <div class="col-md-12">
+            <asp:Table ID="transactionTable" runat="server" CssClass="table table-bordered" autopostback="true">  
+                <asp:TableRow CssClass="table table-hover">
+                    <asp:TableHeaderCell>Transaction ID</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>TRN</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Third Party Company</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Security Question</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Security Answer</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Amount</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Date Sent</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Date Received</asp:TableHeaderCell>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+        <h2 style="text-align: center">Customers</h2>
+        <div class="col-md-12">
+            <asp:Table ID="customerTable" runat="server" CssClass="table table-bordered" autopostback="true">
+                <asp:TableRow CssClass="table table-hover">
+                    <asp:TableHeaderCell>Client TRN</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>First Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Middle Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Last Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Date of Birth</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Address</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Mobile Number</asp:TableHeaderCell>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+        <h2 style="text-align: center">Employees</h2>
+        <div class="col-md-12">
+            <asp:Table ID="EmployeeTable" runat="server" CssClass="table table-bordered" autopostback="true">
+                <asp:TableRow CssClass="table table-hover">
+                    <asp:TableHeaderCell>ID</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>First Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Middle Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Last Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Position</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Date Hired</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Address</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Email</asp:TableHeaderCell>
+                </asp:TableRow>
+            </asp:Table>
         </div>
     </form>
 </body>
--- Advanced_DB_web/Advanced_DB_web/Views/Admin/AdminDashboard.aspx.cs
+++ Advanced_DB_web/Advanced_DB_web/Views/Admin/AdminDashboard.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Advanced_DB_web.Views.Admin
 {
@@ -7,12 +11,191 @@ namespace Advanced_DB_web.Views.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            GetTransactionDataSet();
+            GetCustomerDataSet();
         }
 
         protected void btnLogout_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("~/Default.aspx");
+        }
+
+
+        //Get connection string to database
+        public string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        }
+
+        //Get all transactions in DB
+        protected void GetTransactionDataSet()
+        {
+            string selectSQL = "SELECT * FROM Transactions";
+            SqlConnection transactionConnection = new SqlConnection(GetConnectionString());
+            SqlCommand cmd = new SqlCommand(selectSQL, transactionConnection);
+            SqlDataAdapter transactionAdapter = new SqlDataAdapter(cmd);
+            DataSet dsTransaction = new DataSet();
+            try
+            {
+                transactionConnection.Open();
+                transactionAdapter.Fill(dsTransaction, "Transactions");
+                PopulateTransactionTable(dsTransaction);
+            }
+            catch (Exception err)
+            {
+                lblErrorMessage.Text = err.Message;
+            }
+            finally
+            {
+                transactionConnection.Close();
+            }
+
+        }
+
+        protected void PopulateTransactionTable(DataSet dsTransaction)
+        {
+            foreach (DataRow row in dsTransaction.Tables["Transactions"].Rows)
+            {
+                TableRow transactionTableRow = new TableRow();
+                TableCell transactionTableCells = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_ID"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells);
+
+
+                TableCell transactionTableCells2 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_trn"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells2);
+
+
+                TableCell transactionTableCells4 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_third_party_company"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells4);
+
+
+                TableCell transactionTableCells5 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_security_question"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells5);
+
+                TableCell transactionTableCells7 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_security_answer"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells7);
+
+
+                TableCell transactionTableCells6 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_amount"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells6);
+
+
+                TableCell transactionTableCells8 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_date_sent"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells8);
+
+
+                TableCell transactionTableCells9 = new TableCell
+                {
+                    Text = Convert.ToString(row["Transaction_date_received"])
+                };
+                transactionTableRow.Controls.Add(transactionTableCells9);
+
+                transactionTable.Controls.Add(transactionTableRow);
+
+            }
+        }
+
+        //Get all customers in DB
+        protected void GetCustomerDataSet()
+        {
+            string selectCustomerSQL = "SELECT * FROM Client";
+            SqlConnection sqlConnection = new SqlConnection(GetConnectionString());
+            SqlCommand cmd = new SqlCommand(selectCustomerSQL, sqlConnection);
+            SqlDataAdapter customerSqlDataAdapter = new SqlDataAdapter(cmd);
+            DataSet customerDataSet = new DataSet();
+            try
+            {
+                sqlConnection.Open();
+               customerSqlDataAdapter.Fill(customerDataSet, "Client");
+                PopulateCustomerTable(customerDataSet);
+            }
+            catch (Exception err)
+            {
+                lblErrorMessage.Text = err.Message;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+        }
+
+        
+        protected void PopulateCustomerTable(DataSet dsClient)
+        {
+            foreach (DataRow row in dsClient.Tables["Client"].Rows)
+            {
+                TableRow customerTableRow = new TableRow();
+                TableCell customerTableCell = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_trn"])
+                };
+                customerTableRow.Controls.Add(customerTableCell);
+
+
+                /*TableCell customerTableCell2 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_first_name"])
+                };
+                customerTableRow.Controls.Add(customerTableCell2);
+
+
+                TableCell customerTableCell3 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_middle_name"])
+                };
+                customerTableRow.Controls.Add(customerTableCell3);
+
+
+                TableCell customerTableCell4 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_last_name"])
+                };
+                customerTableRow.Controls.Add(customerTableCell4);
+
+                TableCell customerTableCell5 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_dob"])
+                };
+                customerTableRow.Controls.Add(customerTableCell5);
+
+
+                TableCell customerTableCell6 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_home_address"])
+                };
+                customerTableRow.Controls.Add(customerTableCell6);
+
+
+                TableCell customerTableCell7 = new TableCell
+                {
+                    Text = Convert.ToString(row["Client_mobile_number"])
+                };
+                customerTableRow.Controls.Add(customerTableCell7);
+*/
+
+            }
         }
     }
 }
